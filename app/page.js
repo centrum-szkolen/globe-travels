170abd44b1f04114b60f990bd491c8936ffac820
dawtom97 <daw.tomczyk1997@gmail.com>
2023-09-18 09:24:49 +0200

    apollo client config

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+
+
+
+async function getData() {
+  
+}
+
+
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -5,3 +15,33 @@ export default function Home() {
     </main>
   )
 }
+
+// query MyQuery {
+//   offers {
+//     id
+//     title
+//     slug
+//   }
+// }
+
+
+
+// query Offer {
+//   offer(where:{id:"clmoj9y1dt7490bwb6zo61a5a"}) {
+//     amenities
+//     createdAt
+//     id
+//     images {
+//       url
+//     }
+//     location {
+//       latitude
+//       longitude
+//     }
+//     title
+//     stars
+//     description {
+//       html
+//     }
+//   }
+// }
